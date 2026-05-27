012510b504b0424cde4fbc539b348a1d79bb7e34
RiyaRathod5256 <rr1315576@gmail.com>
2026-05-28 03:31:15 +0530

    remove localhost fallback

--- db.py
+++ db.py
@@ -7,13 +7,13 @@ mysql = MySQL()
 
 def init_db(app):
 
-    app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST', 'localhost')
-    app.config['MYSQL_USER'] = os.getenv('MYSQLUSER', 'root')
-    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD', 'Riya@1234')
-    app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE', 'subscription_mangement')
-    app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT', 3306))
+    app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST')
+    app.config['MYSQL_USER'] = os.getenv('MYSQLUSER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD')
+    app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE')
+    app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT'))
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
     mysql.init_app(app)
-    
 
 
