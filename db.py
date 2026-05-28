79b66663a86bf67f4a8fe30fc96bbaaa322827f9
RiyaRathod5256 <rr1315576@gmail.com>
2026-05-28 05:40:17 +0530

    use MYSQL_URL for railway connection

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
-
-
 import os
 import pymysql
+from urllib.parse import urlparse
 
 def get_connection():
+
+    db_url = urlparse(os.getenv("MYSQL_URL"))
+
     return pymysql.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT")),
+        host=db_url.hostname,
+        user=db_url.username,
+        password=db_url.password,
+        database=db_url.path[1:],
+        port=db_url.port,
         cursorclass=pymysql.cursors.DictCursor
     )
+
